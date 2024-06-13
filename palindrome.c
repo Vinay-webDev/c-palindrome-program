52e08215bdad9cd9ce1ca6260ad0c43ca3d49a3b
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-13 23:09:43 +0530

    .

---
+++ palindrome.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    int NUM,reverse=0,temp,remainder;
+    printf("Enter a number to check if it is a palindrome or not \n");
+    scanf("%d,&NUM);");
+    if(NUM<=999 || NUM>9999)
+    {
+        printf("Enter Four Digit Number \n");
+        return 0;
+
+    }
+    temp=NUM;
+    while(temp!=0)
+    {
+        remainder=temp%10;
+        temp=temp/10;
+        reverse=reverse*10+remainder;
+
+    }
+    printf("Reverse of %d is %d \n",NUM,reverse);
+    if(NUM==reverse)
+    {
+        printf("%d is a palindrome number \n",NUM);
+    }
+    else
+    {
+        prinf("%d is not a palindrome number \n",NUM);
+
+    }
+
+}
